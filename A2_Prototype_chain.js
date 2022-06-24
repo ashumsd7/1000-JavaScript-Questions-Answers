a855ce608865558a2613ff3ab15bdc488049aac9
Ashutosh Anand Tiwari <ashutosh.tiwari@codemonk.in>
2022-06-25 02:37:55 +0530

    Day 2: Prototype chaining

---
+++ A2_Prototype_chain.js
@@ -0,0 +1,7 @@
+// ProtoType chaining is used to build new types of object based on existing object.
+//  It is similar to inheritance in a class based language.
+
+// The prototype on object instance is avaibale through Object.getPrototypeOf(object)
+// or proto property whereas prototype on constructor function is avaialable through Object.prototype
+
+// Learn More: https://www.geeksforgeeks.org/understanding-the-prototype-chain-in-javascript/
